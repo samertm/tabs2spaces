1439727ffcf7027a1049d5716ca253fd71a5927c
Samer Masterson <nosefrog@gmail.com>
2013-06-10 14:22:09 -0400

    Option for number of spaces to expand to

--- tabs2spaces.c
+++ tabs2spaces.c
@@ -3,39 +3,57 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
+    int expandtab = 4;
+    char * filestr;
     if (argc == 1) {
-        printf("usage: tabs2spaces [FILE]\n");
-    } else {
-        FILE *fp = fopen(argv[1], "r");
-        if (fp == NULL) {
-            printf("error: file not found\n");
+        printf("usage: tabs2spaces [OPTIONS] [FILE]\n");
+        printf("options:\n");
+        printf("  -t [NUMBER], --tabs [NUMBER]        The number of spaces you want\n");
+        printf("                                      tabs to expand to.");
+        exit(1);
+    } 
+    int argp = 1;
+    while (argp < argc) {
+        if (strcmp(argv[argp], "-t") == 0 ||
+                strcmp(argv[argp], "--tabs") == 0) {
+            expandtab = atoi(argv[argp + 1]);
+            argp += 2;
         } else {
-            char *tmpstr = malloc(strlen(argv[1]) + 5);
-            strcat(tmpstr, argv[1]);
-            strcat(tmpstr, ".tmp");
-            FILE *tmp = fopen(tmpstr, "w");
-            char c;
-            while ((c = getc(fp)) && c != EOF) {
-                putc(c, tmp);
-            }
-
-            freopen(NULL, "w", fp);
-            freopen(NULL, "r", tmp);
-
-            int i;
-            while ((c = getc(tmp)) && c != EOF) {
-                if (c == '\t') {
-                    for (i = 0; i < 4; i++) {
-                        putc(' ', fp);
-                    }    
-                } else {
-                    putc(c, fp);
-                }
-            }
-            fclose(fp);
-            fclose(tmp);
-            remove(tmpstr);
+            filestr = argv[argp];
+            argp++;
         }
     }
+
+    FILE *fp = fopen(filestr, "r");
+    if (fp == NULL) {
+        printf("error: file not found\n");
+        exit(1);
+    } 
+    char *tmpstr = malloc(strlen(argv[1]) + 5);
+    strcat(tmpstr, argv[1]);
+    strcat(tmpstr, ".tmp");
+    FILE *tmp = fopen(tmpstr, "w");
+    char c;
+    while ((c = getc(fp)) && c != EOF) {
+        putc(c, tmp);
+    }
+
+    freopen(NULL, "w", fp);
+    freopen(NULL, "r", tmp);
+
+    int i;
+    while ((c = getc(tmp)) && c != EOF) {
+        if (c == '\t') {
+            for (i = 0; i < expandtab; i++) {
+                putc(' ', fp);
+            }    
+        } else {
+            putc(c, fp);
+        }
+    }
+    fclose(fp);
+    fclose(tmp);
+    remove(tmpstr);
+
     return 0;
 }
