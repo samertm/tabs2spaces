8b806c2a2568a322c2cabd57a528e40013769545
Samer Masterson <nosefrog@gmail.com>
2013-06-10 13:40:38 -0400

    tabs2spaces now replaces the file
    Used to write to stdout, now replaces the files.

--- tabs2spaces.c
+++ tabs2spaces.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-	if (argc == 1) {
-		printf("usage: tabs2spaces [FILE]\n");
-	} else {
-		FILE *fp = fopen(argv[1], "r");
-		if (fp == NULL) {
-			printf("error: file not found\n");
-		} else {
-			char c;
-			while ((c = getc(fp)) && c != EOF) {
-				if (c == '\t') {
-					printf("    ");
-				} else {
-					printf("%c",c);
-				}
-			}
-			fclose(fp);
-		}
-	}
-	return 0;
+    if (argc == 1) {
+        printf("usage: tabs2spaces [FILE]\n");
+    } else {
+        FILE *fp = fopen(argv[1], "r");
+        if (fp == NULL) {
+            printf("error: file not found\n");
+        } else {
+            char *tmpstr = malloc(strlen(argv[1]) + 5);
+            strcat(tmpstr, argv[1]);
+            strcat(tmpstr, ".tmp");
+            FILE *tmp = fopen(tmpstr, "w");
+            char c;
+            while ((c = getc(fp)) && c != EOF) {
+                putc(c, tmp);
+            }
+
+            freopen(NULL, "w", fp);
+            freopen(NULL, "r", tmp);
+
+            int i;
+            while ((c = getc(tmp)) && c != EOF) {
+                if (c == '\t') {
+                    for (i = 0; i < 4; i++) {
+                        putc(' ', fp);
+                    }    
+                } else {
+                    putc(c, fp);
+                }
+            }
+            fclose(fp);
+            fclose(tmp);
+            remove(tmpstr);
+        }
+    }
+    return 0;
 }
